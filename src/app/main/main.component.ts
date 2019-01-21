b7e8dfe89e95a23763774fba4ad1c917ccc1d9ce
Luke <baslilonfive@gmail.com>
2019-01-21 11:06:02 -0600

    transplanted menu button with innerHTML issues

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -85,7 +85,6 @@ export class MainComponent implements OnInit {
 
         if (st > this.lastScrollTop && st > this.navbarHeight) {
             document.getElementById('logo-nav-box').style.top = '-4vw';
-            document.getElementById('menu').className = 'menu-wrapper animated fadeOutRight';
             this.isMenuOpened = false;
         } else {
             if (st < this.lastScrollTop || st < this.navbarHeight) {
@@ -108,15 +107,15 @@ export class MainComponent implements OnInit {
         }
     }
 
-    openMenu() {
-        if (!this.isMenuOpened) {
-            document.getElementById('menu').style.display = 'flex';
-            document.getElementById('menu').className = 'menu-wrapper animated fadeInRight';
-        } else {
-            document.getElementById('menu').className = 'menu-wrapper animated fadeOutRight';
-        }
-        this.isMenuOpened = !this.isMenuOpened;
-    }
+    // openMenu() {
+    //     if (!this.isMenuOpened) {
+    //         document.getElementById('menu').style.display = 'flex';
+    //         document.getElementById('menu').className = 'menu-wrapper animated fadeInRight';
+    //     } else {
+    //         document.getElementById('menu').className = 'menu-wrapper animated fadeOutRight';
+    //     }
+    //     this.isMenuOpened = !this.isMenuOpened;
+    // }
 
     replaceSpaces(value) {
         if (value === 'email') {
@@ -175,12 +174,12 @@ export class MainComponent implements OnInit {
             document.getElementById('logo-nav-box').style.display = 'flex';
             document.getElementById('logo-nav-box').className = 'logo-nav-box animated fadeInDown';
         }, 100);
-        document.getElementById('menu-btn').onmouseover = () => {
-            document.getElementById('menu-btn').setAttribute('src', 'assets/images/menu-hover.png');
-        };
-        document.getElementById('menu-btn').onmouseout = () => {
-            document.getElementById('menu-btn').setAttribute('src', 'assets/images/menu.png');
-        };
+        // document.getElementById('menu-btn').onmouseover = () => {
+        //     document.getElementById('menu-btn').setAttribute('src', 'assets/images/menu-hover.png');
+        // };
+        // document.getElementById('menu-btn').onmouseout = () => {
+        //     document.getElementById('menu-btn').setAttribute('src', 'assets/images/menu.png');
+        // };
         document.getElementById('logo-nav-box').addEventListener('animationend', () => {
             document.getElementById('header-title').style.display = 'block';
             document.getElementById('header-title').className = 'title animated fadeIn';
